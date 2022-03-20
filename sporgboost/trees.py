ff39796fe1907f9f1b0f30269931c54bc3764f09
Ed Andrews <ed4ndrews@gmail.com>
2022-03-19 20:22:01 -0400

    implemented trees in numba

--- sporgboost/trees.py
+++ sporgboost/trees.py
@@ -1,39 +1,57 @@
-from .common import _predict_tree, _grow_tree
-from .projections import identity, sparse_random, rotation
-from sklearn.base import BaseEstimator
+from sporgboost.common import _predict_tree, _grow_tree
+from sporgboost.projections import identity, sparse_random, rotation
+from numba.experimental import jitclass
+from numba.types import DictType, int64, float64, int64
 
-class BaseDecisionTree(BaseEstimator):
-    def __init__(self, max_depth = None):
-        self.tree = None
-        self.n_classes = None
+dt_spec = [
+    ('tree_value', DictType(int64, float64[:,:])),
+    ('tree_split', DictType(int64, float64)),
+    ('tree_proj', DictType(int64, float64[:,:])),
+    ('n_classes', int64),
+    ('max_depth', int64)
+]
+
+@jitclass(dt_spec)
+class AxisAlignedDecisionTree():
+    def __init__(self, max_depth = 10):
         self.max_depth = max_depth
 
     def fit(self, X, y):
         self.n_classes = y.shape[1]
-        
-    def predict(self, X):
-        return _predict_tree(self.tree, X, self.n_classes)
+        self.tree_value, self.tree_split, self.tree_proj = _grow_tree(X, y, identity, self.max_depth)
 
-class AxisAlignedDecisionTree(BaseDecisionTree):        
-    def fit(self, X, y):
-        super().fit(X, y)
-        self.tree = _grow_tree(X, y, identity, max_depth = self.max_depth)
-        
-class SparseRandomDecisionTree(BaseDecisionTree):
-    def __init__(self, d, s = 3, max_depth = None):
+    def predict(self, X):
+        return _predict_tree(1, self.tree_value, self.tree_split, self.tree_proj, X, self.n_classes)
+
+@jitclass(dt_spec + [
+    ('d', int64),
+    ('s', float64)
+])
+class SparseRandomDecisionTree():
+    def __init__(self, d, s, max_depth = 10):
         self.d = d
         self.s = s
-        super().__init__(max_depth=max_depth)
+        self.max_depth = max_depth
         
     def fit(self, X, y):
-        self.tree = _grow_tree(X, y, sparse_random, d=self.d, s=self.s, max_depth = self.max_depth)
         self.n_classes = y.shape[1]
+        self.tree_value, self.tree_split, self.tree_proj = _grow_tree(X, y, sparse_random, self.max_depth, self.d, self.s)
 
-class RotationalDecisionTree(BaseDecisionTree):
-    def __init__(self, K, max_depth = None):
+    def predict(self, X):
+        return _predict_tree(1, self.tree_value, self.tree_split, self.tree_proj, X, self.n_classes)
+
+
+@jitclass(dt_spec + [
+    ('K', int64)
+])
+class RotationalDecisionTree():
+    def __init__(self, K, max_depth = 10):
         self.K = K
-        super().__init__(max_depth=max_depth)
+        self.max_depth = max_depth
         
     def fit(self, X, y):
-        super().fit(X, y)
-        self.tree = _grow_tree(X, y, rotation, K=self.K, max_depth = self.max_depth)
+        self.n_classes = y.shape[1]
+        self.tree_value, self.tree_split, self.tree_proj = _grow_tree(X, y, rotation, self.max_depth, self.K)
+
+    def predict(self, X):
+        return _predict_tree(1, self.tree_value, self.tree_split, self.tree_proj, X, self.n_classes)
